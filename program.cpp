b8287038d0b04a9c7172688da05673a572cdebf2
Igor <ihor.zhelezniak@mths.ca>
2021-11-09 16:50:33 +0000

    updated

--- program.cpp
+++ program.cpp
@@ -12,7 +12,7 @@ template<size_t N>
 int largestNumberCalculator(std::array<int, N> randomNumbers) {
     // this functions add up all the numbers in the list
 
-    int number = randomNumbers[1];
+    int number = randomNumbers[0];
 
     for (int counter = 0; counter < randomNumbers.size(); counter++) {
         if (randomNumbers[counter] > number) {
@@ -31,17 +31,16 @@ main() {
 
     std::array<int, 10> randomNumbers;
     int randomNumber;
-    int summ = 0;
     int largestNumber;
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());  // mersenne_twister
+    std::uniform_int_distribution<int> idist(-100, 100);  // [0,100]
 
     std::cout << "Starting..." << std::endl;
 
     std::cout << "" << std::endl;
 
     for (int loopCounter = 0; loopCounter < 10; loopCounter++) {
-        std::random_device rseed;
-        std::mt19937 rgen(rseed());  // mersenne_twister
-        std::uniform_int_distribution<int> idist(-100, 100);  // [0,100]
         randomNumber = idist(rgen);
         randomNumbers[loopCounter] = randomNumber;
         std::cout << "The random number " << loopCounter + 1
